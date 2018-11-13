3a19b32304d8966499c84c61f93be46ebfc22b5b
razorcatalyst <mattyptang@gmail.com>
2018-11-13 16:39:19 +0800

    Ready for Heroku

--- Movie.js
+++ Movie.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = 'mongodb://paul:abc123@ds221242.mlab.com:21242/movies';
+const db = 'mongodb://admin:admin12345@ds141043.mlab.com:41043/mattmovies';
 
 mongoose
   .connect(
